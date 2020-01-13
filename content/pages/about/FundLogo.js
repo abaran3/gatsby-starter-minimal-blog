f9ed31091d005d7e57f46122b88db52f18cc81c6
abaran3 <alexik11@hotmail.com>
2020-01-13 19:06:21 +0100

    Add About page

---
+++ content/pages/about/FundLogo.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react"
+
+class FundLogo extends Component {
+  // ------------------------------------------------------
+  // Render
+  // ------------------------------------------------------
+
+  render() {
+      return (
+      // <img
+      //   src="fund_logo.jpg"
+      //   alt="Stimuleringsfonds"
+      //   width="50"
+      //   height="50"
+      // />
+      <div styles={"width: 50%"}> 
+        <img src="fund_logo.jpg" width="200" height="121" alt="logo"/> 
+      </div>
+    )
+  }
+}
+
+export default FundLogo
