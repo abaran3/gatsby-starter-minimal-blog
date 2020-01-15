27a1b2e8cbc15187afc7e1d065baf885934532c6
Jae Perris <jae@kidiyo.com>
2020-01-15 16:45:19 +0100

    Lint Fundlogo.js

--- content/pages/about/FundLogo.js
+++ content/pages/about/FundLogo.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 
 class FundLogo extends Component {
   // ------------------------------------------------------
@@ -6,18 +6,18 @@ class FundLogo extends Component {
   // ------------------------------------------------------
 
   render() {
-      return (
+    return (
       // <img
       //   src="fund_logo.jpg"
       //   alt="Stimuleringsfonds"
       //   width="50"
       //   height="50"
       // />
-      <div styles={"width: 50%"}> 
-        <img src="fund_logo.jpg" width="200" height="121" alt="logo"/> 
+      <div styles={'width: 50%'}>
+        <img src="fund_logo.jpg" width="200" height="121" alt="logo" />
       </div>
-    )
+    );
   }
 }
 
-export default FundLogo
+export default FundLogo;
