b2c4e1daa5c4681fa0db755d3d0a4e70fdc842f8
abaran3 <alexik11@hotmail.com>
2021-02-12 17:14:00 +0100

    Disable gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     // `gatsby-plugin-webpack-bundle-analyser-v2`,
   ],
